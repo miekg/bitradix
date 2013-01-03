d10f22d6d43a069a1530af050fe99e5512ee1571
Miek Gieben <miek@miek.nl>
2013-01-03 18:25:00 +0100

    Make .key .Key and export it

--- bitradix.go
+++ bitradix.go
@@ -44,7 +44,11 @@ func (r *Radix) Remove(n uint64) *Radix {
 // the returned node does not have to be an exact match, it may also be the best
 // matching parent. Comparing r.Key with n tells the caller if the match was exact.
 func (r *Radix) Find(n uint64) *Radix {
+	return r.find(n, 63)
+}
 
+func (r *Radix) String() string {
+	return r.str("")
 }
 
 // Implement insert
@@ -55,10 +59,10 @@ func (r *Radix) insert(n uint64, v uint32, bit uint) *Radix {
 		// Internal node, no key. With branches, walk the branches.
 		return r.branch[bitK(n, bit)].insert(n, v, bit-1)
 	case false:
-		// external node, (optional) key, no branches
+		// External node, (optional) key, no branches
 		if !r.keyset {
 			r.keyset = true
-			r.key = n
+			r.Key = n
 			r.Value = v
 			return r
 		}
@@ -68,35 +72,44 @@ func (r *Radix) insert(n uint64, v uint32, bit uint) *Radix {
 
 		r.internal = true
 		r.keyset = false
-		bcur := bitK(r.key, bit)
+		bcur := bitK(r.Key, bit)
 		bnew := bitK(n, bit)
 
 		if bcur == bnew {
 			// "fill" the correct node, with the current key - and call ourselves
-			r.branch[bcur].key = r.key
+			r.branch[bcur].Key = r.Key
 			r.branch[bcur].keyset = true
-			r.key = 0
+			r.Key = 0
 			return r.branch[bcur].insert(n, v, bit-1)
 		}
 		// bcur = 0, bnew == 1 or vice versa
-		r.branch[bcur].key = r.key
+		r.branch[bcur].Key = r.Key
 		r.branch[bcur].keyset = true
-		r.branch[bnew].key = n
+		r.branch[bnew].Key = n
 		r.branch[bnew].keyset = true
-		r.key = 0
+		r.Key = 0
 		return r.branch[bnew]
 	}
 	panic("bitradix: not reached")
 }
 
-func (r *Radix) String() string {
-	return r.str("")
+func (r *Radix) find(n uint64, bit uint) *Radix {
+	// if bit == 0, return the current node?? Also see comment in r.insert()
+	switch r.internal {
+	case true:
+		// Internal node, no key
+		return r.branch[bitK(n, bit)].find(n, bit-1)
+	case false:
+		// External node, (optional) key, no branches
+		return r
+	}
+	panic("bitradix: not reached")
 }
 
 func (r *Radix) str(indent string) (s string) {
 	s += indent
 	if r.keyset {
-		s += strconv.Itoa(int(r.key)) + "\n" + indent
+		s += strconv.Itoa(int(r.Key)) + "\n" + indent
 	} else {
 		s += "<nil>\n" + indent
 	}
